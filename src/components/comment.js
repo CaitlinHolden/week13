e559a52d508edccc5784c7ece5fd2c8694912ad6
Caitlin H <92063389+CaitlinHolden@users.noreply.github.com>
2022-02-04 19:44:30 -0500

    Add files via upload

---
+++ src/components/comment.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReplyButton from './reply-button';
+import LikeButton from './like-button';
+import { render } from '@testing-library/react';
+
+let e = React.createElement;
+
+export default class Comment extends React.Component {
+    render() {
+    return e('div',
+        {class: 'card w75'},
+    e('div', 
+         {class: 'card-header bg-success text-white'},
+        'Username and Time'),
+     e('div',
+        {class: 'card-body'},
+        'Comment Content'),
+    e('div',
+    {class: 'card-footer'},
+        e(LikeButton, {}, null),
+        e('span', {}, null),
+        e(ReplyButton, {}, null)
+    )
+    );
+    }
+}
